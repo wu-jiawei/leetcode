746af48930366c35dd627795c07ae70ffa20f512
Jiawei Wu <cchaoyao810@gmail.com>
2018-11-16 18:48:04 +0800

    110-first-solution

---
+++ 110-balanced-binary-tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+class TreeNode:
+     def __init__(self, x):
+         self.val = x
+         self.left = None
+         self.right = None
+
+class Solution:
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if root is None:
+            return True
+        if abs(self.depth(root.left) - self.depth(root.right)) > 1:
+            return False
+        return self.isBalanced(root.left) and self.isBalanced(root.right)
+
+    def depth(self, root):
+        """
+        Return depth of a binary tree rooted at root
+        """
+        if root is None:
+            return 0
+        return max(self.depth(root.left), self.depth(root.right)) + 1
